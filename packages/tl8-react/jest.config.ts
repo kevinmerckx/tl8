468bd88cdd521dfcc0c61b5a2f9a4478c66af590
Kevin Merckx <kmpeachdev@gmail.com>
2024-03-29 08:25:55 +0100

    Move tl8-react under packages

--- packages/tl8-react/jest.config.ts
+++ packages/tl8-react/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/tl8-react',
+  coverageDirectory: '../../coverage/packages/tl8-react',
 };
